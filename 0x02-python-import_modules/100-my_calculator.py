6944cffce7d32c53cfdd5ea8dafdfdf3413ff1bb
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2022-06-04 06:49:42 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-from sys import argv
 from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
 
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    result = add(int(argv[1]), int(argv[3]))
-elif argv[2] == '-':
-    result = sub(int(argv[1]), int(argv[3]))
-elif argv[2] == '*':
-    result = mul(int(argv[1]), int(argv[3]))
-elif argv[2] == '/':
-    result = div(int(argv[1]), int(argv[3]))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
 
-print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3], result))
+def arg_calc(argv):
+    n = len(argv) - 1
+    if n != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    op = argv[2]
+    b = int(argv[3])
+    if op == '+':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+    elif op == '-':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+    elif op == '*':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+    elif op == '/':
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+
+if __name__ == "__main__":
+    import sys
+    arg_calc(sys.argv)
