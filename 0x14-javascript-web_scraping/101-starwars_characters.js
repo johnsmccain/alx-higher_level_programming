7422849e8f8511c54ca32733106002405fa6c5eb
johnsmccain <johnsdanlami@gmail.com>
2022-10-18 18:48:28 +0100

    a script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,16 +2,26 @@
 // A script that prints all characters of a Star Wars movie
 
 const request = require('request');
-const id = process.argv[2];
-if (!id) return console.log("try this: ./101-starwars_characters.js 3")
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+function printCharacters (characters, idx) {
+	request(characters[idx], (err, res, body) => {
+		if (err) {
+			console.log(err);
+	          } else {
+			console.log(JSON.parse(body).name);
+			if (idx + 1 < characters.length) {
+				printCharacters(characters, idx + 1);
+			}
+		}
+	});
+}
 
 request(url, (err, res, body) => {
-	const characters = JSON.parse(body).characters;
-	//console.log(characters[1].split("/"))
-	characters.forEach(ch => {
-		request(ch, (err, res, body) => {
-			console.log(JSON.parse(body).name)
-		})
-	})
-})
+	if (err) {
+		console.log(err);
+	} else {
+		const characters = JSON.parse(body).characters;
+		printCharacters(characters, 0);
+	}
+});
