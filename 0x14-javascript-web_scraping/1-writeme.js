425598fa2c320ce0f718ad592a7d2c7ff934c8ee
johnsmccain <johnsdanlami@gmail.com>
2022-10-18 19:53:55 +0100

    fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 // A script that write a string to a file.
 
 const fs = require('fs');
-const file = process.argv[2]
+const file = process.argv[2];
 const text = process.argv[3];
 
 fs.writeFile(file, text, 'utf-8', (err) => {
