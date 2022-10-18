6121813fb4b1de5fb519a140cf303492669f2d36
johnsmccain <johnsdanlami@gmail.com>
2022-10-18 11:10:31 +0100

    Revert "first solution"
    This reverts commit efb061d207606bbc95fe912418dc479b1dd9cd96.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// A script display the status code of a GET request
-
-const request = require('request');
-const URL = process.argv[2];
-
-request(URL, (err, res, body) => {
-	console.log("code: ", res.statusCode);
-})
