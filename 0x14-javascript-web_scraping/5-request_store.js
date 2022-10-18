b1f9f19f11f4652c106025fbc545c9162722c9b6
johnsmccain <johnsdanlami@gmail.com>
2022-10-18 19:55:46 +0100

    fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,9 +3,11 @@
 
 const fs = require('fs');
 const request = require('request');
-const [_, _1, url, file] = process.argv;
+const url = process.argv[2];
+const file = process.argv[3];
 
 request(url, (err, res, body) => {
+  if (err) return console.log(err);
   fs.writeFile(file, body, 'utf-8', (err) => {
     if (err) console.log(err);
   });
